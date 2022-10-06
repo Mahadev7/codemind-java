38c6519e478ecc5e66d6dcd3513fed66b779c9af
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-10-06 12:27:00 +0530

    //Farenheat to celsius value import java.util.Scanner; class Cels { public static void main(String args[]) { Scanner sc=new Scanner(System.in); int a=sc.nextInt(); double b=32+(a*1.8); System.out.format("%.2f",b); } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+//Farenheat to celsius value
+import java.util.Scanner;
+class Cels
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+double b=32+(a*1.8);
+System.out.format("%.2f",b);
+}
+}
