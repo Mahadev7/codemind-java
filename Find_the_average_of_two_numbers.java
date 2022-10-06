038008e56d69b801b0cc50b73672855f490a0990
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-10-06 13:15:21 +0530

    import java .util.Scanner; class Example { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); double a=Sc.nextInt(); double b=Sc.nextInt(); double d=(a+b)/2; System.out.printf("%.4f",d); } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java .util.Scanner;
+class Example
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+double a=Sc.nextInt();
+double b=Sc.nextInt();
+double d=(a+b)/2;
+System.out.printf("%.4f",d);
+}
+}
