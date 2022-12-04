2469b14ca311b0db904f5a42b3549833badbd4da
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-12-04 13:05:53 +0530

    import java.util.Scanner; class Char30 { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); char c=Sc.next().charAt(0); int a=c; System.out.println(a); } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Char30
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+char c=Sc.next().charAt(0);
+int a=c;
+System.out.println(a);
+}
+}
