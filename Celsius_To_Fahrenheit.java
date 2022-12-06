a17b47cce3be83482777029c1d1e5ac33a9c8d5e
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-12-07 00:37:19 +0530

    import java.util.Scanner; class cel { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int a=Sc.nextInt(); double p=(1.8*a)+32; System.out.format("%.2f",p); } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class cel
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int a=Sc.nextInt();
+double p=(1.8*a)+32;
+System.out.format("%.2f",p);
+}
+}
