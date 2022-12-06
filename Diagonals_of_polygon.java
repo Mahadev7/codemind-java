68990badfc8bda08cca354e32de2952c21fc9c3d
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-12-07 00:32:11 +0530

    import java.util.Scanner; class poly { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int a=Sc.nextInt(); int p=a*(a-3)/2; System.out.println(p); } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class poly
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int a=Sc.nextInt();
+int p=a*(a-3)/2;
+System.out.println(p);
+}
+}
