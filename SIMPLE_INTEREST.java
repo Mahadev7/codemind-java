ddcb85851ea936c2433da3b51c19d5e46f32114b
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2022-12-08 19:17:58 +0530

    import java.util.Scanner; class Si { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int p=Sc.nextInt(); int r=Sc.nextInt(); int t=Sc.nextInt(); int si=(p*r*t)/100; System.out.println(si); } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Si
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int p=Sc.nextInt();
+int r=Sc.nextInt();
+int t=Sc.nextInt();
+int si=(p*r*t)/100;
+System.out.println(si);
+}
+}
