3aab19b4a29b824f7039776e79c62cdc59a982e5
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-01-01 11:24:44 +0530

    import java.util.Scanner; class Technical { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); String p=Sc.nextLine(); System.out.println("Hello Technicalhub"); System.out.println(p); } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Technical
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+String p=Sc.nextLine();
+System.out.println("Hello Technicalhub");
+System.out.println(p);
+}
+}
