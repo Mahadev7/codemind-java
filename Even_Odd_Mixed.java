87af7de9abf1f526e0b4821696e6c28d2d927d5b
21MH1A04Q1 <98527340+Mahadev7@users.noreply.github.com>
2023-01-07 10:51:37 +0530

    import java.util.Scanner; class even71 { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int n=Sc.nextInt(); String a=Integer.toString(n); int l=a.length(); int e=0; int o=0; int p=0; while (n>0) { 	p=n%10; 	if (p%2==0) 	{ 		e+=1; 	} 	else 	{ 		o+=1; 	} 	n=n/10; } if (e==l) { 	System.out.println("Even"); } else if(o==l) { 	System.out.println("Odd"); } else { 	System.out.println("Mixed"); } } }

---
+++ Even_Odd_Mixed.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class even71
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+String a=Integer.toString(n);
+int l=a.length();
+int e=0;
+int o=0;
+int p=0;
+while (n>0)
+{
+	p=n%10;
+	if (p%2==0)
+	{
+		e+=1;
+	}
+	else
+	{
+		o+=1;
+	}
+	n=n/10;
+}
+if (e==l)
+{
+	System.out.println("Even");
+}
+else if(o==l)
+{
+	System.out.println("Odd");
+}
+else
+{
+	System.out.println("Mixed");
+}
+}
+}
